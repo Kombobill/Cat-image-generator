1bf888183d5ded73c6098d090c89589ff3fdb23a
Kombobill <118278282+Kombobill@users.noreply.github.com>
2022-12-19 23:31:39 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -40,6 +40,26 @@ post.addEventListener("click", function(){
  
 });
 
+const likeBtn = document.querySelector(".like_btn");
+const likeIcon = document.querySelector("#icon");
+const count = document.querySelector("#count");
+
+
+let clicked = false;
+
+
+likeBtn.addEventListener("click", () =>{
+    if(!clicked) {
+        clicked = true;
+        likeIcon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+        count.textContent++;
+    }else {
+        clicked = false;
+        likeIcon.innerHTML = `i class="far fa-thumbs-up"></i>`;
+        count.textContent--;
+    }
+});
+
 
 
 
